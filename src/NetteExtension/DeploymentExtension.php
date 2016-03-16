b5fad13d0c10bfc5d57a2782483f5b2e1255ad98
Jaroslav Hranička <hranicka@outlook.com>
2016-03-16 13:17:52 +0100

    Deployment: No-environment setup support added

--- src/NetteExtension/DeploymentExtension.php
+++ src/NetteExtension/DeploymentExtension.php
@@ -36,75 +36,82 @@ class DeploymentExtension extends DI\CompilerExtension
 
 	public function loadConfiguration()
 	{
-		$builder = $this->getContainerBuilder();
+		if ($this->config['environments']) {
+			foreach ($this->config['environments'] as $name => $path) {
+				$environmentConfig = $this->loadFromFile($path)[$this->name];
+				$config = DI\Config\Helpers::merge($this->getContainerBuilder()->expand($environmentConfig), $this->config);
 
-		if (!$this->config['environments']) {
-			throw new \RuntimeException('No Environment is configured.');
-		}
+				$this->addEnvironment($name, $config);
+			}
 
-		foreach ($this->config['environments'] as $name => $path) {
-			$environmentConfig = $this->loadFromFile($path)[$this->name];
-			$config = DI\Config\Helpers::merge($this->getContainerBuilder()->expand($environmentConfig), $this->config);
-
-			// Normalize & expand config
-			$config['jobs'] = array_map(function ($v) {
-				return $this->expandJobs((array)$v, $this->config);
-			}, $config['jobs']);
-
-			$builder
-				->addDefinition($this->prefixEnvironment($name, 'jobs'))
-				->setClass(Deployment\Jobs\Jobs::class, [$config['jobs']])
-				->setAutowired(FALSE);
-
-			$builder
-				->addDefinition($this->prefixEnvironment($name, 'server'))
-				->setClass(Deployment\Server\FtpServer::class, [$config['ftp']])
-				->setAutowired(FALSE);
-
-			$builder
-				->addDefinition($this->prefixEnvironment($name, 'collector'))
-				->setClass(Deployment\FileCollector::class, [
-					[
-						'path' => $config['paths']['local'],
-						'ignore' => $config['paths']['ignore'],
-					],
-				])
-				->setAutowired(FALSE);
-
-			$builder
-				->addDefinition($this->prefixEnvironment($name, 'fileList'))
-				->setClass(Deployment\FileList::class)
-				->setAutowired(FALSE);
-
-			$builder
-				->addDefinition($this->prefixEnvironment($name, 'deployer'))
-				->setClass(Deployment\Deployer::class, [
-					[
-						'path' => $config['paths']['remote'],
-						'temp' => '/.deploy/',
-						'deployedFile' => '/.deployed',
-						'deletedFile' => '/.deleted',
-					],
-					'@' . $this->prefixEnvironment($name, 'server'),
-					'@' . $this->prefixEnvironment($name, 'collector'),
-					'@' . $this->prefixEnvironment($name, 'fileList'),
-				])
-				->setAutowired(FALSE);
-
-			$builder
-				->addDefinition($this->prefixEnvironment($name, 'deployment'))
-				->setClass(Deployment\SymfonyConsole\DeploymentCommand::class, [
-					'deployment:' . $name,
-				])
-				->addSetup('setJobs', ['@' . $this->prefixEnvironment($name, 'jobs')])
-				->addSetup('setDeployer', ['@' . $this->prefixEnvironment($name, 'deployer')])
-				->addTag('kdyby.console.command');
+		} else {
+			$this->addEnvironment('', $this->config);
 		}
 	}
 
-	private function prefixEnvironment($env, $id)
+	private function addEnvironment($name, array $config)
 	{
-		return $this->prefix($id . '.' . $env);
+		$builder = $this->getContainerBuilder();
+
+		// Normalize & expand config
+		$config['jobs'] = array_map(function ($v) {
+			return $this->expandJobs((array)$v, $this->config);
+		}, $config['jobs']);
+
+		$builder
+			->addDefinition($this->prefixEnvironment('jobs', $name))
+			->setClass(Deployment\Jobs\Jobs::class, [$config['jobs']])
+			->setAutowired(FALSE);
+
+		$builder
+			->addDefinition($this->prefixEnvironment('server', $name))
+			->setClass(Deployment\Server\FtpServer::class, [$config['ftp']])
+			->setAutowired(FALSE);
+
+		$builder
+			->addDefinition($this->prefixEnvironment('collector', $name))
+			->setClass(Deployment\FileCollector::class, [
+				[
+					'path' => $config['paths']['local'],
+					'ignore' => $config['paths']['ignore'],
+				],
+			])
+			->setAutowired(FALSE);
+
+		$builder
+			->addDefinition($this->prefixEnvironment('fileList', $name))
+			->setClass(Deployment\FileList::class)
+			->setAutowired(FALSE);
+
+		$builder
+			->addDefinition($this->prefixEnvironment('deployer', $name))
+			->setClass(Deployment\Deployer::class, [
+				[
+					'path' => $config['paths']['remote'],
+					'temp' => '/.deploy/',
+					'deployedFile' => '/.deployed',
+					'deletedFile' => '/.deleted',
+				],
+				'@' . $this->prefixEnvironment('server', $name),
+				'@' . $this->prefixEnvironment('collector', $name),
+				'@' . $this->prefixEnvironment('fileList', $name),
+			])
+			->setAutowired(FALSE);
+
+		$builder
+			->addDefinition($this->prefixEnvironment('deployment', $name))
+			->setClass(Deployment\SymfonyConsole\DeploymentCommand::class, [
+				implode(':', array_filter(['deployment', $name])),
+			])
+			->addSetup('setJobs', ['@' . $this->prefixEnvironment('jobs', $name)])
+			->addSetup('setDeployer', ['@' . $this->prefixEnvironment('deployer', $name)])
+			->addTag('kdyby.console.command');
+	}
+
+	private function prefixEnvironment($id, $name)
+	{
+		$names = array_filter([$id, $name]);
+		return $this->prefix(implode('.', $names));
 	}
 
 	private function expandJobs(array $jobs, array $config)
